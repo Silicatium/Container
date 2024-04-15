416770089a85670b484f82c1d9a3cc6eea8e64f9
Marat <yanbaev.marat2004@mail.ru>
2024-04-15 09:58:31 +0500

    Добавьте файлы проекта.

---
+++ Container/Container.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <class AbstractClass>
+class Container {
+private:
+	int size;
+	AbstractClass* array;
+public:
+	Container() : size(0), array(nullptr) {}
+	Container(int size) : size(size), array(new AbstractClass[size]) {}
+	Container(const Container& с) : size(с.size), array(new AbstractClass[с.size]) {
+		for (int i = 0; i < size; i++) {
+			array[i] = с.array[i];
+		}
+	}
+	~Container() {
+		delete[] array;
+	}
+};
+
+
+int main() {
+
+}
