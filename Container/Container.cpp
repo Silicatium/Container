4192d96a56a7a5a377a40faf3691182eebb121b5
Marat <yanbaev.marat2004@mail.ru>
2024-04-16 06:14:51 +0500

    Добавление метода insert и исправление коснтруктора по умолчанию.

--- Container/Container.cpp
+++ Container/Container.cpp
@@ -8,7 +8,7 @@ private:
 	int size;
 	AbstractClass** array;
 public:
-	Container() : size(0), array(new AbstractClass * []) {}
+	Container() : size(0), array(new AbstractClass * [0]) {}
 	Container(int size) : size(size), array(new AbstractClass * [size]) {
 		for (int i = 0; i < size; i++) {
 			array[i] = new AbstractClass();
@@ -42,7 +42,7 @@ public:
 		}
 	}
 	void push_back(AbstractClass element) {
-		size += 1;
+		size++;
 		AbstractClass** temp_arr = new AbstractClass * [size];
 		for (int i = 0; i < size - 1; i++) {
 			temp_arr[i] = new AbstractClass(*array[i]);
@@ -58,7 +58,7 @@ public:
 		delete[] temp_arr;
 	}
 	void push_front(AbstractClass element) {
-		size += 1;
+		size++;
 		AbstractClass** temp_arr = new AbstractClass * [size];
 		temp_arr[0] = new AbstractClass(element);
 		for (int i = 1; i < size; i++) {
@@ -73,18 +73,41 @@ public:
 		}
 		delete[] temp_arr;
 	}
+	void insert(int index, AbstractClass element) {
+		int parametr = 0; //for negative index
+		if (index < 0) parametr = size;
+		if (index + parametr < size) {
+			size++;
+			AbstractClass** temp_arr = new AbstractClass * [size];
+			for (int i = 0; i < index + parametr; i++) {
+				temp_arr[i] = new AbstractClass(*array[i]);
+				delete array[i];
+			}
+			temp_arr[index + parametr] = new AbstractClass(element);
+			for (int i = index + parametr + 1; i < size; i++) {
+				temp_arr[i] = new AbstractClass(*array[i - 1]);
+				delete array[i - 1];
+			}
+			delete[] array;
+			array = new AbstractClass * [size];
+			for (int i = 0; i < size; i++) {
+				array[i] = new AbstractClass(*temp_arr[i]);
+				delete temp_arr[i];
+			}
+			delete[] temp_arr;
+		}
+		else this->push_back(element);
+	}
 };
 
 
 int main() {
 
 	Container<int> c(4);
-	c.push_front(1);
+	c.insert(2, 3);
 	for (int i = 0; i < c.get_size(); i++) {
 		cout << c.get_object(i) << endl;
 	}
-	c.set_object(2, 4);
-	cout << c.get_object(2);
 
 	return 0;
 }
