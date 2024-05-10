ff2f9cba44ca40f6ff3395977fcf041e5a816711
Marat <yanbaev.marat2004@mail.ru>
2024-05-11 00:25:51 +0500

    Добавлены параметры памяти для избегания быстрого форматирования памяти и связанные методы. Также добавлен метод pop.

--- Container/Container.cpp
+++ Container/Container.cpp
@@ -9,20 +9,22 @@ template <class AbstractClass>
 class Container {
 private:
 	int size;
+	int memory;
 	AbstractClass** array;
 	void checked_empty_container() {
 		if (size == 0) throw exception("Container is empty!");
 	}
+	int additional_memory = 50; //memory increase
 public:
-	Container() : size(0), array(new AbstractClass * [0]) {}
-	Container(int size) : size(size), array(new AbstractClass * [size]) {
+	Container() : size(0), memory(0), array(new AbstractClass* [0]) {}
+	Container(int size) : size(size), memory(size), array(new AbstractClass* [size]) {
 		for (int i = 0; i < size; i++) {
 			array[i] = new AbstractClass();
 		}
 	}
-	Container(const Container& с) : size(с.size), array(new AbstractClass * [с.size]) {
+	Container(const Container& c) : size(c.size), memory(c.size), array(new AbstractClass* [c.memory]) {
 		for (int i = 0; i < size; i++) {
-			array[i] = new AbstractClass(*(с.array[i]));
+			array[i] = new AbstractClass(*(c.array[i]));
 		}
 	}
 	~Container() {
@@ -33,6 +35,9 @@ public:
 	}
 	int get_size() {
 		return size;
+	}
+	int get_additional_memory() {
+		return additional_memory;
 	}
 	AbstractClass get_object(int index) {
 		//Return element in container
@@ -54,24 +59,32 @@ public:
 			*array[index + parametr] = element;
 		}
 	}
+	void set_additional_memory(int quantity) {
+		additional_memory = quantity;
+	}
 	void push_back(AbstractClass element) {
 		size++;
-		AbstractClass** temp_arr = new AbstractClass * [size];
-		for (int i = 0; i < size - 1; i++) {
-			temp_arr[i] = new AbstractClass(*array[i]);
-			delete array[i];
+		if (memory >= size) array[size - 1] = new AbstractClass(element);
+		else {
+			memory += additional_memory;
+			AbstractClass** temp_arr = new AbstractClass * [size];
+			for (int i = 0; i < size - 1; i++) {
+				temp_arr[i] = new AbstractClass(*array[i]);
+				delete array[i];
+			}
+			temp_arr[size - 1] = new AbstractClass(element);
+			delete[] array;
+			array = new AbstractClass * [memory];
+			for (int i = 0; i < size; i++) {
+				array[i] = new AbstractClass(*temp_arr[i]);
+				delete temp_arr[i];
+			}
+			delete[] temp_arr;
 		}
-		temp_arr[size - 1] = new AbstractClass(element);
-		delete[] array;
-		array = new AbstractClass * [size];
-		for (int i = 0; i < size; i++) {
-			array[i] = new AbstractClass(*temp_arr[i]);
-			delete temp_arr[i];
-		}
-		delete[] temp_arr;
 	}
 	void push_front(AbstractClass element) {
 		size++;
+		if (memory < size) memory += additional_memory;
 		AbstractClass** temp_arr = new AbstractClass * [size];
 		temp_arr[0] = new AbstractClass(element);
 		for (int i = 1; i < size; i++) {
@@ -79,7 +92,7 @@ public:
 			delete array[i - 1];
 		}
 		delete[] array;
-		array = new AbstractClass * [size];
+		array = new AbstractClass * [memory];
 		for (int i = 0; i < size; i++) {
 			array[i] = new AbstractClass(*temp_arr[i]);
 			delete temp_arr[i];
@@ -91,6 +104,7 @@ public:
 		if (index < 0) parametr = size;
 		if (index + parametr >= 0 && index + parametr < size) {
 			size++;
+			if (memory < size) memory += additional_memory;
 			AbstractClass** temp_arr = new AbstractClass * [size];
 			for (int i = 0; i < index + parametr; i++) {
 				temp_arr[i] = new AbstractClass(*array[i]);
@@ -102,7 +116,7 @@ public:
 				delete array[i - 1];
 			}
 			delete[] array;
-			array = new AbstractClass * [size];
+			array = new AbstractClass * [memory];
 			for (int i = 0; i < size; i++) {
 				array[i] = new AbstractClass(*temp_arr[i]);
 				delete temp_arr[i];
@@ -110,6 +124,10 @@ public:
 			delete[] temp_arr;
 		}
 		else this->push_back(element);
+	}
+	void pop() {
+		size--;
+		delete array[size];
 	}
 	void remove(int index) {
 		int parametr = 0; //for negative index
@@ -127,7 +145,7 @@ public:
 				delete array[i];
 			}
 			delete[] array;
-			array = new AbstractClass * [size];
+			array = new AbstractClass * [memory];
 			for (int i = 0; i < size; i++) {
 				array[i] = new AbstractClass(*temp_arr[i]);
 				delete temp_arr[i];
@@ -209,6 +227,7 @@ int main() {
 	int countOfActions;
 	int randomEvent;
 	random_create_objects(c);
+	cout << "Enter count of actions: ";
 	cin >> countOfActions;
 	system_clock::time_point startTime = system_clock::now();
 	for (int i = 0; i < countOfActions; i++) {
